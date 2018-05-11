7e3b76d5ec47a9d05e375c8ed658ec94c942b5fa
josetome95 <jose1995jose1995@gmail.com>
2018-05-11 12:27:12 +0200

    P5_Quiz

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,24 @@
 var express = require('express');
+var {models} = require('../models/index');
 var router = express.Router();
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize("sqlite:quizzes.sqlite", {logging: false});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
 router.get('/credits', function(req, res, next) {
-  res.render('credits', { title: 'Express' });
+  res.render('credits');
+});
+router.get('/quizzes', function(req, res, next) {
+	models.quiz.findAll().
+	then(quizzes=>{
+ res.render('quizzes',{quizzes});})
+       
+	
+ 
 });
 
-router.get('/quizzes', function(req, res, next) {
-  res.render('quizzes', { title: 'Express' });
-});
+
 module.exports = router;
